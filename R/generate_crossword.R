3462504f43c34236e24859c840cec63bee965c86
petermeissner <retep.meissner@gmail.com>
2018-09-15 15:29:04 +0200

    init

---
+++ R/generate_crossword.R
@@ -0,0 +1,23 @@
+xword_gen <- function(words, clues = words, tries = 10){
+  
+  # push data to V8
+  v8_env$v8$assign("words", words)
+  v8_env$v8$assign("clues", clues)
+  v8_env$v8$assign("tries", tries)
+  
+  # initiate new Crossword Object
+  v8_env$v8$eval("var cw = new Crossword(words, clues);")
+  
+  # compute grid
+  v8_env$v8$eval("var grid = cw.getSquareGrid(tries);")
+  
+  # retrieve result
+  res <- 
+    structure(
+      v8_env$v8$get("grid", simplifyVector = FALSE),
+      class = c("xword_js", "xword", "list")
+    )
+  
+  # return
+  res
+}
