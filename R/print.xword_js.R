9347c5b3f6504406ecc83348e9913df9b2e9309e
petermeissner <retep.meissner@gmail.com>
2018-09-15 16:36:32 +0200

    passing checks

--- R/print.xword_js.R
+++ R/print.xword_js.R
@@ -1,8 +1,8 @@
 #' print.xword_js
 #'
 #' @param x data to transformed
-#'
+#' @param ... not used, only here to mirror generic
 #' @export
-print.xword_js <- function(x){
+print.xword_js <- function(x, ...){
  print(as.matrix(x))
 }
